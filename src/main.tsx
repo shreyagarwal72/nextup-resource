0c77ab1093137a858dce089073c46774e27c0aa6
Vanshu Agarwal <sanjayvansu1973@gmail.com>
2026-08-09 01:03:14 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { setupPWA } from "./lib/pwa";
+import { initSentry } from "./lib/sentry";
+
+initSentry();
 
 createRoot(document.getElementById("root")!).render(<App />);
 
